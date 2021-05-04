020b0bdcbc0b135875c466722d41a505e88539e9
Raphael Huet de Froberville <rdefrob@gmail.com>
2021-05-04 10:36:05 +0200

    Début du projet

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="shortcut icon" href="img/GBAF-favicon.png" type="image/x-icon">
+    <title>GBAF</title>
+</head>
+<header>
+<div class="en-tete">
+        <img src="img/GBAF-logo.jpg" alt="logo-gbaf" title="Groupement Banque Assurance Français">
+        <div class="connexion">
+            <img src="https://github.com/google/material-design-icons/blob/master/android/action/perm_identity/materialicons/black/res/drawable-hdpi/baseline_perm_identity_black_36.png?raw=true" alt="connect">
+            <p>Connexion</p>
+        </div>
+</div>
+</header>
+<body>
+    <div class="hero">
+        <h1>Extranet du Groupement Banque Assurance Français</h1>
+        <p>La Fédération bancaire française est l’organisation professionnelle qui représente toutes les banques installées en France, et un lobby. Elle compte 340 entreprises bancaires adhérentes de toutes tailles françaises ou étrangères dont 115 banques étrangères.</p>
+
+        
+        <!-- <img class src="https://images.unsplash.com/photo-1462206092226-f46025ffe607?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1953&q=80" alt="fédération-bancaire-francaise"> -->
+    </div>
+
+
+</body>
+</html>
+
