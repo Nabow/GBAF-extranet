470e46f350c161ef864ffc6bb9ce5e2e0c14e24a
Raphael Huet de Froberville <rdefrob@gmail.com>
2021-06-21 16:31:46 +0200

    ajout du dotenv

--- core/connect_bdd.php
+++ core/connect_bdd.php
@@ -1,9 +1,8 @@
 <?php 
-require_once "config.php";
-
+require_once "../config.php";
 
 function connectBdd(){
-    return new PDO('mysql:host='.BDD_HOST.';dbname='.BDD_DATABASE.';charset=utf8;port='. BDD_PORT, BDD_IDENTIFIANT, BDD_PASSWORD, [
+    return new PDO('mysql:host='.$_ENV["BDD_HOST"].';dbname='.$_ENV["BDD_DATABASE"].';charset=utf8;port='. $_ENV["BDD_PORT"], $_ENV["BDD_IDENTIFIANT"], $_ENV["BDD_PASSWORD"], [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
         PDO::ATTR_EMULATE_PREPARES=>false
