4ad41bfe605f4ecf56ae05c7580876305438db51
Raphael Huet de Froberville <rdefrob@gmail.com>
2021-06-21 17:06:44 +0200

    Nouvelle correction

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php 
 require_once 'vendor/autoload.php';
 use Symfony\Component\Dotenv\Dotenv;
+
 $dotenv = new Dotenv();
-// you can also load several files
-$dotenv->load(__DIR__.'/.env', __DIR__.'/.env.dev');
+$dotenv->loadEnv(__DIR__.'/.env');
